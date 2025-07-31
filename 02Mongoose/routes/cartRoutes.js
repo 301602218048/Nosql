4aa29f0cb3c322508facff95b218c67f4c14dc84
301602218048 <abhinav.sharma2901@gmail.com>
2025-08-01 02:45:37 +0530

    delete cart

--- 02Mongoose/routes/cartRoutes.js
+++ 02Mongoose/routes/cartRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const addCart = require("../controllers/cartController");
+const { addCart, deleteCartItem } = require("../controllers/cartController");
 
 const router = express.Router();
 
 router.post("/addCart/:id", addCart);
+cartRouter.delete("/deleteCartItem/:id", deleteCartItem);
 // router.get("/getCart/:id", findCartById);
 
 module.exports = router;
